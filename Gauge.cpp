504a1f9d1cf778fce10375d45410331d9c576ab7
nitorionedan <b1444263@planet.kanazawa-it.ac.jp>
2016-07-21 22:45:41 +0900

    fix Gauge.cpp

--- Gauge.cpp
+++ Gauge.cpp
@@ -2,26 +2,27 @@
 #include "Field.hpp"
 
 
-	Gauge::Gauge()
-	{
-		AllDeletePoint = 0; //全消しするために貯めるポイント
-	}
+Gauge::Gauge()
+{
+	AllDeletePoint = 0; //全消しするために貯めるポイント
+}
 
-	Gauge::~Gauge()
-	{
+Gauge::~Gauge()
+{
 
-	}
+}
 
-	void Gauge::AllDelete() //全消しポイントが100ポイント貯まると、任意のタイミングで特定の1面を全消しできる
+void Gauge::AllDelete() //全消しポイントが100ポイント貯まると、任意のタイミングで特定の1面を全消しできる
+{
+	for (int y = 0; y < Field::FieldHeight; y++)
 	{
-		for (int y = 0; y < Field::FieldHeight; y++)
+		for (int x = 0; x < Field::FieldWidth; x++)
 		{
-			for (int x = 0; x < Field::FieldWidth; x++)
-			{
-				Field::FieldMino[x][y] = 0; //テトリスフィールド上の判定を全て消去
-				Field::FieldColor[x][y] = 0; //テトリスフィールドのテトリミノの色を消去
-
+			Field::FieldMino[x][y] = 0; //テトリスフィールド上の判定を全て消去
+			Field::FieldColor[x][y] = 0; //テトリスフィールドのテトリミノの色を消去
 			}
-		}
-		AllDeletePoint = 0;
 	}
+
+
+	AllDeletePoint = 0;
+}
